3d9a8adeee2b87b7dd5d56bd199bdce17b126ab7
Hossein Sadeghi <sadeghihossein.hs@gmail.com>
2025-01-16 16:31:36 +0330

    feat: enhance player listing to include associated tags
    - Updated the listPlayers function to return a list of players along with their associated tags.
    - Introduced a new PlayersListResponse type to structure the response data.
    - Modified SQL query to join Players, PlayerTags, and Tags tables for comprehensive player data retrieval.

--- customer-support-backend/src/models/player.model.ts
+++ customer-support-backend/src/models/player.model.ts
@@ -1,6 +1,9 @@
 import db from "../config/database";
 import { addTag } from "../repositories/tag.repo";
 import { addPlayerTag } from "../repositories/playerTag.repo";
+import { PlayerDTO } from "../repositories/player.repo";
+
+type PlayersListResponse = PlayerDTO & { tags: string[] };
 
 export const createPlayer = (name: string, tagNames: string[]) => {
   const stmt = db.prepare("INSERT INTO Players (name) VALUES (?)");
@@ -17,6 +20,16 @@ export const getPlayerById = (id: number) => {
   return db.prepare("SELECT * FROM Players WHERE id = ?").get(id);
 };
 
-export const listPlayers = () => {
-  return db.prepare("SELECT * FROM Players").all();
+export const listPlayers = (): PlayersListResponse[] => {
+  return db
+    .prepare(
+      `
+    SELECT Players.id, Players.name, GROUP_CONCAT(Tags.name) AS tags
+    FROM Players
+    LEFT JOIN PlayerTags ON Players.id = PlayerTags.player_id
+    LEFT JOIN Tags ON PlayerTags.tag_id = Tags.id
+    GROUP BY Players.id
+  `
+    )
+    .all() as PlayersListResponse[];
 };
