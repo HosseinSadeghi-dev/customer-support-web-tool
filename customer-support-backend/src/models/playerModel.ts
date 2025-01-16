5c8b5bd0edce1c45db79603a15016359dc9897c1
Hossein Sadeghi <sadeghihossein.hs@gmail.com>
2025-01-16 14:36:55 +0330

    refactor: streamline player creation and tag management
    - Refactored createPlayer function to utilize addTag and addPlayerTag for tag handling.
    - Enhanced addTag function to check for existing tags before insertion.
    - Introduced findTagByName function to retrieve tags by name, improving tag management efficiency.
    - Improved error handling for tag existence during player-tag association.

--- customer-support-backend/src/models/playerModel.ts
+++ customer-support-backend/src/models/playerModel.ts
@@ -1,31 +1,26 @@
 import db from "../config/database";
 import { TagDTO } from "../repositories/tag.repo";
+import { addTag } from "../repositories/tag.repo";
+import { addPlayerTag } from "../repositories/playerTag.repo";
 
 export const createPlayer = (name: string, tagNames: string[]) => {
   const stmt = db.prepare("INSERT INTO Players (name) VALUES (?)");
   const result = stmt.run(name);
-  const playerId = result.lastInsertRowid;
-
-  const tagStmt = db.prepare(
-    "INSERT INTO Tags (name) VALUES (?) ON CONFLICT(name) DO NOTHING"
-  );
-  const playerTagStmt = db.prepare(
-    "INSERT INTO PlayerTags (player_id, tag_id) VALUES (?, ?)"
-  );
+  const playerId = result.lastInsertRowid as number;
 
   tagNames.forEach((tagName: string) => {
-    tagStmt.run(tagName);
+    addTag(tagName);
+
     const tagResult = db
       .prepare<Partial<TagDTO>, TagDTO>("SELECT id FROM Tags WHERE name = ?")
       .get({ name: tagName });
 
-    // Check if the tag exists
     if (!tagResult) {
       throw new Error(`Tag "${tagName}" does not exist.`);
     }
 
     const tagId = tagResult.id;
-    playerTagStmt.run(playerId, tagId);
+    addPlayerTag(playerId, tagId);
   });
 
   return playerId;
--- customer-support-backend/src/repositories/tag.repo.ts
+++ customer-support-backend/src/repositories/tag.repo.ts
@@ -13,6 +13,9 @@ export const createTagsTable = `
 `;
 
 export const addTag = (name: string): TagDTO => {
+  if (tagExists(name)) {
+    return findTagByName(name);
+  }
   const stmt = db.prepare("INSERT INTO Tags (name) VALUES (?)");
   const info = stmt.run(name);
   return { id: Number(info.lastInsertRowid), name };
@@ -22,4 +25,13 @@ export const tagExists = (name: string): boolean => {
   const stmt = db.prepare("SELECT COUNT(*) AS count FROM Tags WHERE name = ?");
   const row = stmt.get(name) as { count: number };
   return row.count > 0;
+};
+
+export const findTagByName = (name: string): TagDTO | null => {
+  const stmt = db.prepare("SELECT * FROM Tags WHERE name = ?");
+  const row = stmt.get({ name }) as TagDTO | null;
+  if (row) {
+    return { id: row.id, name: row.name };
+  }
+  return null;
 };
